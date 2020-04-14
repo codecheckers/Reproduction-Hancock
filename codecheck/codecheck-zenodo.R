a3f1917bf65173fd7601df2779cf4591d84880cc
Stephen J. Eglen <sje30@cam.ac.uk>
2020-04-14 09:56:11 +0100

    Example update to handle multuple codecheckers

--- codecheck/codecheck-zenodo.R
+++ codecheck/codecheck-zenodo.R
@@ -7,21 +7,31 @@ require(yaml)
 yaml_file = "../codecheck.yml"
 codecheck_yaml = read_yaml( yaml_file) 
 
-## The following line ma work only for SJE
+## The following line may work only for SJE
 my_token = system("pass show codechecker-token", intern=TRUE)
 
 zenodo <- ZenodoManager$new(
-   token = my_token,
-   logger = "INFO"
-)
+   ##url = "http://sandbox.zenodo.org/api",
+   token = my_token)
 
-##myrec <- zenodo$createEmptyRecord()
+
+## Codechecker may have already created a draft report, but if not let's
+## do it now. 
+this_doi = codecheck_yaml$report
+if (length(this_doi)==0) {
+  ## run this only once.  perhaps after creating it, stick the doi in the yml file?
+  myrec <- zenodo$createEmptyRecord()
+  ## capture the DOI and store it in the yml file, as we don't want to create a new one.
+  this_doi = myrec$metadata$prereserve_doi$doi
+  print(this_doi)
+}
+
 
 
 ## 10.5281/zenodo.3746024
 
 # get draft (manually created)
-deposit_draft <- zenodo$getDepositionByConceptDOI("10.5281/zenodo.3746024")
+deposit_draft <- zenodo$getDepositionByConceptDOI(this_doi)
 
 # add metadata
 deposit_draft$setPublicationType("report")
@@ -31,8 +41,23 @@ deposit_draft$setTitle(paste("CODECHECK certificate", codecheck_yaml$certificate
 # could also add codechecker as author
 # deposit_draft$addContributor(), but need to split firstname/lastname, handle optiona ORCID etc.
 deposit_draft$setLanguage(language = "eng")
-deposit_draft$setLanguage(language = "eng")
-deposit_draft$setDescription(paste("CODECHECK certificate for paper:", codecheck_yaml$paper$title))
+
+
+## for uploading the creators, addCreator() will simply add another, potentially duplicate entry,
+## so we should remove all creators before providing just the one we want.
+deposit_draft$metadata$creators = NULL
+num_creators = length(codecheck_yaml$codechecker)
+for (i in 1:num_creators) {
+  deposit_draft$addCreator(
+                  firstname = codecheck_yaml$codechecker[[i]]$firstname,
+                  lastname  = codecheck_yaml$codechecker[[i]]$lastname,
+                  orcid     = codecheck_yaml$codechecker[[i]]$ORCID)
+}
+
+description_text = paste("CODECHECK certificate for paper:", codecheck_yaml$paper$title)
+repo_url = gsub("[<>]", "", codecheck_yaml$repository)
+description_text = paste(description_text, sprintf('<p><p>Repository: <a href="%s">%s</a>', repo_url, repo_url))
+deposit_draft$setDescription(description_text)
 deposit_draft$setKeywords(keywords = c("CODECHECK"))
 deposit_draft$setNotes(notes = c("See file LICENSE for license of the contained code. The report document codecheck.pdf is published under CC-BY 4.0 International."))
 deposit_draft$setAccessRight(accessRight = "open")
@@ -46,6 +71,8 @@ deposit_draft <- zenodo$depositRecord(deposit_draft)
 ## Upload the file itself when ready.
 zenodo$uploadFile("codecheck.pdf", deposit_draft$id)
 
+
+## now go to zenodo and check the record.
 
 ## Created an zenodo token with permission to do everything using:
 ## https://zenodo.org/account/settings/applications/tokens/new/
